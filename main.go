bcaaf4714003f3bf2dc89ca21871db50a4c24ade
Jacqueline Kendi <kendyjaky@gmail.com>
2021-04-01 02:50:10 +0300

    App running fine

--- main.go
+++ main.go
@@ -1,38 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
+	"io"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+// Route declaration
+func router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	return r
+}
+
+// Initiate web server
 func main() {
-	// reqeusts to the path /goodbye with be handled by this function
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World")
-	})
+	router := router()
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:9100",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
 
-	// any other request will be handled by this function
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Running Hello Handler")
-
-		// read the body
-		b, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println("Error reading body", err)
-
-			http.Error(rw, "Unable to read request body", http.StatusBadRequest)
-			return
-		}
-
-		// write the response
-		fmt.Fprintf(rw, "Hello %s", b)
-	})
-
-	// Listen for connections on all ip addresses (0.0.0.0)
-	// port 9090
-	log.Println("Starting Server")
-	err := http.ListenAndServe(":9090", nil)
-	log.Fatal(err)
+	log.Fatal(srv.ListenAndServe())
 }
