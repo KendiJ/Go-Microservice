596cfd5892f9050a5c130d14e510e4ea5fe54a81
Jacqueline Kendi <kendyjaky@gmail.com>
2021-04-22 00:51:59 +0300

    Basic Web Server in Go

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
-	"io"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
-}
 
-// Route declaration
-func router() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler)
-	return r
-}
+func main () {
 
-// Initiate web server
-func main() {
-	router := router()
-	srv := &http.Server{
-		Handler: router,
-		Addr:    "127.0.0.1:9100",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello")
+		d, err := ioutil.ReadAll(r.Body)
+		if err !=  nil {
+			http.Error(rw, "Ooopsy", http.StatusBadRequest)
+			return
+		}
 
-	log.Fatal(srv.ListenAndServe())
+		// log.Printf("Data %s\n", d) //Able to print data to the log
+
+		//write back to the user.
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("See you later Self:)")
+	})
+
+
+	http.ListenAndServe(":9090", nil)
 }
